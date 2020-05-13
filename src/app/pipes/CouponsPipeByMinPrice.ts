86bde26f9deb70bff7bc973c5c597e949733b356
DanielFinis <dpines7@gmail.com>
2020-05-13 15:44:48 +0300

    pipe disebled

--- src/app/pipes/CouponsPipeByMinPrice.ts
+++ src/app/pipes/CouponsPipeByMinPrice.ts
@@ -1,19 +1,20 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Coupon } from '../models/Coupon';
+// import { Pipe, PipeTransform } from '@angular/core';
+// import { Coupon } from '../models/coupon';
 
-@Pipe({
-    name: 'minPricePipe'
-})
-export class CouponsPipeByMinPrice implements PipeTransform {
 
-    transform(coupons: Coupon[], maxPrice:number): any {
+// @Pipe({
+//     name: 'minPricePipe'
+// })
+// export class CouponsPipeByMinPrice implements PipeTransform {
 
-        // Filter is very similary forEach
-        // Filter automaticallty goes through each item in the array, sends it to a predicate
-        // A predicate is a big word to a "condition". If the predicate returns true -> 
-        // the item enters to the filtered array, if false --> the item will not be in the filtered array.
-        //-----------
-        // In this example we filter all coupons whose price is about 350
-        return coupons.filter(coupon => coupon.price<maxPrice);
-    }
-}
+//     transform(coupons: Coupon[], maxPrice:number): any {
+
+//         // Filter is very similary forEach
+//         // Filter automaticallty goes through each item in the array, sends it to a predicate
+//         // A predicate is a big word to a "condition". If the predicate returns true -> 
+//         // the item enters to the filtered array, if false --> the item will not be in the filtered array.
+//         //-----------
+//         // In this example we filter all coupons whose price is about 350
+//         return coupons.filter(coupon => coupon.price<maxPrice);
+//     }
+// }
